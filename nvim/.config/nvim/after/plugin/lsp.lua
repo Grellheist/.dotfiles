5461960d4f0b957203e3db92dbb901f418fdf216
Grellheist <grellheist@tutanota.com>
2023-04-25 14:08:03 -0300

    Added snippets for react

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -19,12 +19,15 @@ lsp.configure('lua-language-server', {
 
 
 local cmp = require('cmp')
+require("luasnip").filetype_extend("javascript", {"javascriptreact"})
+require("luasnip").filetype_extend("javascript", {"html"})
+
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
