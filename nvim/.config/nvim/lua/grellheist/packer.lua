efc5bb5e89d260c0a8207c0bb97a142891df865f
Grellheist <grellheist@tutanota.com>
2023-05-09 17:10:26 -0300

    Added cursorline and barbar

--- nvim/.config/nvim/lua/grellheist/packer.lua
+++ nvim/.config/nvim/lua/grellheist/packer.lua
@@ -4,84 +4,104 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use({
-		'catppuccin/nvim',
-		as = 'catppuccin',
-		config = function()
-			vim.cmd [[colorscheme catppuccin]]
-		end
-	})
-	use {
-		"folke/trouble.nvim",
-		requires = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("trouble").setup {}
-		end
-	}
+    use({
+        'catppuccin/nvim',
+        as = 'catppuccin',
+        config = function()
+            vim.cmd [[colorscheme catppuccin]]
+        end
+    })
+    use {
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {}
+        end
+    }
 
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-	use('nvim-treesitter/playground')
-	use('ThePrimeagen/harpoon')
-	use('mbbill/undotree')
-	use('vim-airline/vim-airline')
-	use('vim-airline/vim-airline-themes')
-	use('lervag/vimtex')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('vim-airline/vim-airline')
+    use('vim-airline/vim-airline-themes')
+    use('lervag/vimtex')
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'saadparwaiz1/cmp_luasnip' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			{ 'rafamadriz/friendly-snippets' },
-		}
-	}
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
-	use {
-		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
-	}
-	use "lukas-reineke/indent-blankline.nvim"
-	use "brenoprata10/nvim-highlight-colors"
-	-- use "mg979/vim-visual-multi"
-	-- use "github/copilot.vim"
-	use "tpope/vim-commentary"
-	use "tpope/vim-fugitive"
-	use "ThePrimeagen/vim-be-good"
-	use({
-		"kylechui/nvim-surround",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end
-	})
-	use({
-		"m4xshen/smartcolumn.nvim",
-		config = function ()
-			require("smartcolumn").setup()
-		end
-	})
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+    use "lukas-reineke/indent-blankline.nvim"
+    use "brenoprata10/nvim-highlight-colors"
+    -- use "mg979/vim-visual-multi"
+    -- use "github/copilot.vim"
+    use "tpope/vim-commentary"
+    use "tpope/vim-fugitive"
+    use "ThePrimeagen/vim-be-good"
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+    use({
+        "m4xshen/smartcolumn.nvim",
+        config = function()
+            require("smartcolumn").setup()
+        end
+    })
+    use { 'romgrk/barbar.nvim', requires = {
+        'lewis6991/gitsigns.nvim'
+    } }
+    use({
+        'yamatsum/nvim-cursorline',
+        config = function()
+            require('nvim-cursorline').setup {
+                cursorline = {
+                    enable = true,
+                    timeout = 1000,
+                    number = false,
+                },
+                cursorword = {
+                    enable = true,
+                    min_length = 3,
+                    hl = { underline = true },
+                }
+            }
+        end
+    })
 end)
