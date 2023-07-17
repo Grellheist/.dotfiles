f0d32754bc2f96dab82ce26362e8ac24104f6b3a
Grellheist <grellheist@tutanota.com>
2023-07-17 12:19:09 -0300

    ignoring node_modules in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,9 @@
 require("grellheist.remap")
 require("grellheist.set")
+require('telescope').setup {
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules"
+        }
+    }
+}
