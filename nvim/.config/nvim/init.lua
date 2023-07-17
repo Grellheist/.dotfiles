3285fc56e01c0902bb15e0e97d3f7c0089a17a64
Grellheist <grellheist@tutanota.com>
2023-07-17 12:38:49 -0300

    added hidden files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,14 @@
 require("grellheist.remap")
 require("grellheist.set")
 require('telescope').setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    },
     defaults = {
         file_ignore_patterns = {
-            "node_modules"
+            "node_modules", "build", "dist"
         }
     }
 }
