1001862494583400422e339f295ad1c5c7f90365
Victor Hugo <grellheist@tutanota.com>
2023-10-17 19:24:37 -0300

    updated fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -207,41 +207,11 @@ alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
 
-# Color output of ip
-alias ip="ip -color"
-
 #readable output
 alias df="df -h"
 
-#keyboard
-alias give-me-azerty-be="sudo localectl set-x11-keymap be"
-alias give-me-qwerty-us="sudo localectl set-x11-keymap us"
-
-#setlocale
-alias setlocale="sudo localectl set-locale LANG=en_US.UTF-8"
-alias setlocales="sudo localectl set-x11-keymap be && sudo localectl set-locale LANG=en_US.UTF-8"
-
-#pacman unlock
-alias unlock="sudo rm /var/lib/pacman/db.lck"
-alias rmpacmanlock="sudo rm /var/lib/pacman/db.lck"
-
-#arcolinux logout unlock
-alias rmlogoutlock="sudo rm /tmp/arcologout.lock"
-
-#which graphical card is working
-alias whichvga="/usr/local/bin/arcolinux-which-vga"
-
-#free
-alias free="free -mt"
-
 #continue download
 alias wget="wget -c"
-
-#userlist
-alias userlist="cut -d: -f1 /etc/passwd | sort"
-
-#merge new settings
-alias merge="xrdb -merge ~/.Xresources"
 
 # Aliases for software managment
 # pacman
@@ -251,98 +221,6 @@ alias update="sudo pacman -Syu"
 # paru as aur helper - updates everything
 alias pksyua="paru -Syu --noconfirm"
 alias upall="paru -Syu --noconfirm"
-
-#ps
-alias psa="ps auxf"
-alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
-
-#grub update
-alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-#grub issue 08/2022
-alias install-grub-efi="sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi"
-
-#add new fonts
-alias update-fc="sudo fc-cache -fv"
-
-#copy/paste all content of /etc/skel over to home folder - backup of config created - beware
-#skel alias has been replaced with a script at /usr/local/bin/skel
-
-#backup contents of /etc/skel to hidden backup folder in home/user
-alias bupskel="cp -Rf /etc/skel ~/.skel-backup-(date +%Y.%m.%d-%H.%M.%S)"
-
-#copy shell configs
-alias cb="cp /etc/skel/.bashrc ~/.bashrc && echo "Copied.""
-alias cz="cp /etc/skel/.zshrc ~/.zshrc && echo "Copied.""
-alias cf="cp /etc/skel/.config/fish/config.fish ~/.config/fish/config.fish && exec fish"
-
-#switch between bash, zsh and fish
-alias tobash="sudo chsh $USER -s /bin/bash && echo 'Done. Now log out.'"
-alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Done. Now log out.'"
-alias tofish="sudo chsh $USER -s /bin/fish && echo 'Done. Now log out.'"
-
-#switch between lightdm and sddm
-alias tolightdm="sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed ; sudo systemctl enable lightdm.service -f ; echo 'Lightm is active - reboot now'"
-alias tosddm="sudo pacman -S sddm --noconfirm --needed ; sudo systemctl enable sddm.service -f ; echo 'Sddm is active - reboot now'"
-alias toly="sudo pacman -S ly --noconfirm --needed ; sudo systemctl enable ly.service -f ; echo 'Ly is active - reboot now'"
-alias togdm="sudo pacman -S gdm --noconfirm --needed ; sudo systemctl enable gdm.service -f ; echo 'Gdm is active - reboot now'"
-alias tolxdm="sudo pacman -S lxdm --noconfirm --needed ; sudo systemctl enable lxdm.service -f ; echo 'Lxdm is active - reboot now'"
-
-# kill commands
-# quickly kill conkies
-alias kc="killall conky"
-# quickly kill polybar
-alias kp="killall polybar"
-# quickly kill picom
-alias kpi="killall picom"
-
-#hardware info --short
-alias hw="hwinfo --short"
-
-#audio check pulseaudio or pipewire
-alias audio="pactl info | grep 'Server Name'"
-
-#skip integrity check
-alias paruskip="paru -S --mflags --skipinteg"
-alias yayskip="yay -S --mflags --skipinteg"
-alias trizenskip="trizen -S --skipinteg"
-
-#check vulnerabilities microcode
-alias microcode="grep . /sys/devices/system/cpu/vulnerabilities/*"
-
-#get fastest mirrors in your neighborhood
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-alias mirrord="sudo reflector --latest 30 --number 10 --sort delay --save /etc/pacman.d/mirrorlist"
-alias mirrors="sudo reflector --latest 30 --number 10 --sort score --save /etc/pacman.d/mirrorlist"
-alias mirrora="sudo reflector --latest 30 --number 10 --sort age --save /etc/pacman.d/mirrorlist"
-#our experimental - best option for the moment
-alias mirrorx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 5 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
-alias mirrorxx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 20 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
-alias ram="rate-mirrors --allow-root --disable-comments arch | sudo tee /etc/pacman.d/mirrorlist"
-alias rams="rate-mirrors --allow-root --disable-comments --protocol https arch  | sudo tee /etc/pacman.d/mirrorlist"
-
-#mounting the folder Public for exchange between host and guest on virtualbox
-alias vbm="sudo /usr/local/bin/arcolinux-vbox-share"
-
-#enabling vmware services
-alias start-vmware="sudo systemctl enable --now vmtoolsd.service"
-alias vmware-start="sudo systemctl enable --now vmtoolsd.service"
-alias sv="sudo systemctl enable --now vmtoolsd.service"
-
-
-#youtube download
-alias yta-aac="yt-dlp --extract-audio --audio-format aac"
-alias yta-best="yt-dlp --extract-audio --audio-format best"
-alias yta-flac="yt-dlp --extract-audio --audio-format flac"
-alias yta-mp3="yt-dlp --extract-audio --audio-format mp3"
-alias ytv-best="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4"
-
-#Recent Installed Packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
-alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
-
-#iso and version used to install ArcoLinux
-alias iso="cat /etc/dev-rel | awk -F '=' '/ISO/ {print $2}'"
-alias isoo="cat /etc/dev-rel"
 
 #Cleanup orphaned packages
 alias cleanup="sudo pacman -Rns (pacman -Qtdq)"
@@ -408,66 +286,6 @@ alias fix-keys="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
 alias fix-pacman-conf="/usr/local/bin/arcolinux-fix-pacman-conf"
 alias fix-pacman-keyserver="/usr/local/bin/arcolinux-fix-pacman-gpg-conf"
 
-#maintenance
-alias big="expac -H M "%m\t%n" | sort -h | nl"
-alias downgrada="sudo downgrade --ala-url https://ant.seedhost.eu/arcolinux/"
-
-#hblock (stop tracking with hblock)
-#use unhblock to stop using hblock
-alias unhblock="hblock -S none -D none"
-
-#systeminfo
-alias probe="sudo -E hw-probe -all -upload"
-alias sysfailed="systemctl list-units --failed"
-
-#shutdown or reboot
-alias ssn="sudo shutdown now"
-alias sr="sudo reboot"
-
-#update betterlockscreen images
-alias bls="betterlockscreen -u /usr/share/backgrounds/arcolinux/"
-
-#give the list of all installed desktops - xsessions desktops
-alias xd="ls /usr/share/xsessions"
-alias xdw="ls /usr/share/wayland-sessions"
-
-#wayland aliases
-alias wsimplescreenrecorder="wf-recorder -a"
-
-#btrfs aliases
-alias btrfsfs="sudo btrfs filesystem df /"
-alias btrfsli="sudo btrfs su li / -t"
-
-#snapper aliases
-alias snapcroot="sudo snapper -c root create-config /"
-alias snapchome="sudo snapper -c home create-config /home"
-alias snapli="sudo snapper list"
-alias snapcr="sudo snapper -c root create"
-alias snapch="sudo snapper -c home create"
-
-#Leftwm aliases
-alias lti="leftwm-theme install"
-alias ltu="leftwm-theme uninstall"
-alias lta="leftwm-theme apply"
-alias ltupd="leftwm-theme update"
-alias ltupg="leftwm-theme upgrade"
-
-#arcolinux applications
-#att is a symbolic link now
-#alias att="archlinux-tweak-tool"
-alias adt="arcolinux-desktop-trasher"
-alias abl="arcolinux-betterlockscreen"
-alias agm="arcolinux-get-mirrors"
-alias amr="arcolinux-mirrorlist-rank-info"
-alias aom="arcolinux-osbeck-as-mirror"
-alias ars="arcolinux-reflector-simple"
-alias atm="arcolinux-tellme"
-alias avs="arcolinux-vbox-share"
-alias awa="arcolinux-welcome-app"
-
-#remove
-alias rmgitcache="rm -r ~/.cache/git"
-
 #moving your personal files and folders from /personal to ~
 function personal
     cp -rf /personal/ ~
@@ -477,12 +295,12 @@ end
 # git
 # using plugin
 # omf install https://github.com/jhillyerd/plugin-git
-alias undopush "git push -f origin HEAD^:master"
+# alias undopush "git push -f origin HEAD^:master"
 #alias add="git add -A"
 #alias commit="git commit -m "Updated""
 #alias push="git push"
-alias gtpush="git add -A;git commit -m "Updated";git push"
-alias tbpush="git add -A;git commit -m 'To be implemented';git push"
+# alias gtpush="git add -A;git commit -m "Updated";git push"
+# alias tbpush="git add -A;git commit -m 'To be implemented';git push"
 
 # Neovim
 alias vim="nvim"
